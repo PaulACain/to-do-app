12151a98c9bc64a2f67b2203e2eded4caed52292
Paul <paul.audet.cain@gmail.com>
2019-02-15 10:22:32 -0500

    checkpoint_in_progress

--- app.js
+++ app.js
@@ -7,50 +7,31 @@ function onReady() {
   addToDoForm.addEventListener('submit', () => {
   event.preventDefault();
 
-    // get the text
-    let title = newToDoText.value;
+      // get the text
+      let title = newToDoText.value;
 
-    //create a new li
-    let newLi = document.createElement('li');
+      // create a new li
+      let newLi = document.createElement('li');
 
-    //create new input
-    let checkbox = document.createElement('input');
+      // create a new input
+      let checkbox = document.createElement('input');
 
-    //set the input's type to checkbox
-    checkbox.type ="checkbox";
+      // set the input's type to checkbox
+      checkbox.type = "checkbox";
 
-    // set the title
-    newLi.textContent = title;
+      // set the title
+      newLi.textContent = title;
 
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
+      // attach the checkbox to the li
+      newLi.appendChild(checkbox);
 
-    // get the text
-    let title = newToDoText.value;
+      // attach the li to the ul
+      toDoList.appendChild(newLi);
 
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
-    newToDoText.value = '';
+      //empty the input
+      newToDoText.value = '';
      });
 }
-
 
 window.onload = function() {
 onReady();
