36e5ffe40ae6a43598eaa80552fb9475e95178c4
Paul <paul.audet.cain@gmail.com>
2019-02-22 17:14:30 -0500

    checpoint_complete

--- app.js
+++ app.js
@@ -3,27 +3,12 @@ function onReady() {
   const toDos= [];
   const addToDoForm = document.getElementById('addToDoForm')
 
-  function createNewToDo(){
-    const newToDoText = document.getElementsById('newToDoText');
-    if (!newToDoText.value) {return; }
-
-    toDos.push({
-      title: newToDoText.value,
-      complete: false
-    })
-  }
-
-  newToDoText.value = '';
-
-  renderTheUI();
-}
-
   function renderTheUI(){
     const toDoList = document.getElementById('toDoList');
 
     toDoList.textContent = '';
 
-    toDo.forEach(function(toDo){
+    toDos.forEach(function(toDo){
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
@@ -40,7 +25,22 @@ function onReady() {
     createNewToDo();
   });
 
+  function createNewToDo(){
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) {return; }
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    })
+    renderTheUI();
+  }
+
+  newToDoText.value = '';
+
   renderTheUI();
+}
+
 
 window.onload = function() {
 onReady();
